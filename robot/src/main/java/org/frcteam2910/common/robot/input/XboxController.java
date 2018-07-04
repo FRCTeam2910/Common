2f544cba71ce7064bc8d81ef6c83ac4f448f2077
Jacob Bublitz <jacob@bublitz.com>
2018-07-03 23:50:43 -0700

    Make slight changes to the javadocs to make them not error

--- robot/src/main/java/org/frcteam2910/common/robot/input/XboxController.java
+++ robot/src/main/java/org/frcteam2910/common/robot/input/XboxController.java
@@ -5,14 +5,13 @@ import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
- * <p>IGamepad implementation for XBox-like gamepads.</p>
+ * IGamepad implementation for XBox-like gamepads.
  * <p>
- * <p>Currently known to work with:
+ * Currently known to work with:
  * <ul>
  * <li>Xbox 360 wired controller</li>
  * <li>Logitech F310</li>
  * </ul>
- * </p>
  *
  * @author Jacob Bublitz
  * @since 1.0
--- src/main/java/com/team254/lib/util/Interpolable.java
+++ src/main/java/com/team254/lib/util/Interpolable.java
@@ -11,14 +11,14 @@ package com.team254.lib.util;
 public interface Interpolable<T> {
     /**
      * Interpolates between this value and an other value according to a given parameter. If x is 0, the method should
-     * return this value. If x is 1, the method should return the other value. If 0 < x < 1, the return value should be
+     * return this value. If x is 1, the method should return the other value. If 0 &lt; x &lt; 1, the return value should be
      * interpolated proportionally between the two.
      *
      * @param other
      *            The value of the upper bound
      * @param x
      *            The requested value. Should be between 0 and 1.
-     * @return Interpolable<T> The estimated average between the surrounding data
+     * @return Interpolable&lt;T&lt; The estimated average between the surrounding data
      */
     public T interpolate(T other, double x);
 }
