49d6f22ae8449455b5307b5dc6a195c522ab9161
Jacob Bublitz <jacob@bublitz.com>
2018-07-03 23:51:37 -0700

    Add Rotation and RigidTransform classes
    Still need to do:
    * Finish documenting Vector2 and RigidTransform2 classes
    * Remove all uses of now-deprecated Vector2 methods

---
+++ src/main/java/org/frcteam2910/common/math/RigidTransform2.java
@@ -0,0 +1,43 @@
+package org.frcteam2910.common.math;
+
+public class RigidTransform2 {
+    public Vector2 translation;
+    public Rotation2 rotation;
+
+    public RigidTransform2(Vector2 translation, Rotation2 rotation) {
+        this.translation = translation;
+        this.rotation = rotation;
+    }
+
+    private static Vector2 intersectionInternal(RigidTransform2 a, RigidTransform2 b) {
+        final double t = ((a.translation.x - b.translation.x) * b.rotation.tan + b.translation.y - a.translation.y) /
+                (a.rotation.sin - a.rotation.cos * b.rotation.tan);
+        return a.translation.add(Vector2.fromAngle(a.rotation).scale(t));
+    }
+
+    public RigidTransform2 transformBy(RigidTransform2 other) {
+        return new RigidTransform2(translation.add(other.translation.rotateBy(rotation)), rotation.rotateBy(other.rotation));
+    }
+
+    public RigidTransform2 inverse() {
+        Rotation2 inverseRotation = rotation.inverse();
+        return new RigidTransform2(translation.inverse().rotateBy(inverseRotation), inverseRotation);
+    }
+
+    public Vector2 intersection(RigidTransform2 other) {
+        if (rotation.isParallel(other.rotation)) {
+            return new Vector2(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+        }
+
+        if (Math.abs(rotation.cos) < Math.abs(other.rotation.cos)) {
+            return intersectionInternal(this, other);
+        } else {
+            return intersectionInternal(other, this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "{T: " + translation + ", R: " + rotation + "}";
+    }
+}
--- src/main/java/org/frcteam2910/common/math/Vector2.java
+++ src/main/java/org/frcteam2910/common/math/Vector2.java
@@ -1,5 +1,8 @@
 package org.frcteam2910.common.math;
 
+import java.text.DecimalFormat;
+import java.util.Objects;
+
 import static org.frcteam2910.common.math.MathUtils.epsilonEquals;
 
 /**
@@ -12,6 +15,11 @@ public final class Vector2 {
 	public final double length;
 	public final double angle;
 
+	/**
+	 * Create a vector at (0, 0).
+	 *
+	 * @deprecated Use {@link Vector2#ZERO} instead.
+	 */
 	public Vector2() {
 		this(0, 0);
 	}
@@ -25,6 +33,13 @@ public final class Vector2 {
 		this.angle = Math.atan2(y, x);
 	}
 
+	/**
+	 * Copy a vector.
+	 * @param other The vector to make a copy of.
+	 *
+	 * @deprecated Vectors are immutable, there is no reason to make copies.
+	 */
+	@Deprecated
 	public Vector2(Vector2 other) {
 		this.x = other.x;
 		this.y = other.y;
@@ -32,12 +47,40 @@ public final class Vector2 {
 		this.angle = other.angle;
 	}
 
+	/**
+	 * Create a new unit vector with the specified angle.
+	 * @param angle The angle of the new unit vector.
+	 * @return A unit vector with the specified angle.
+	 *
+	 * @deprecated Use {@link #fromAngle(Rotation2)} instead.
+	 */
+	@Deprecated
 	public static Vector2 fromAngle(double angle) {
 		return new Vector2(Math.cos(angle), Math.sin(angle));
 	}
 
-	public Vector2 add(double scalar) {
-		return add(scalar, scalar);
+	public static Vector2 fromAngle(Rotation2 rotation) {
+		return new Vector2(rotation.cos, rotation.sin);
+	}
+
+	/**
+	 * Calculate the angle between two vectors.
+	 *
+	 * @param a The vector that the angle begins at.
+	 * @param b The vector that the angle ends at.
+	 * @return The angle between the two vectors.
+	 */
+	public static Rotation2 getAngleBetween(Vector2 a, Vector2 b) {
+		double cos = a.dot(b) / (a.length * b.length);
+		if (Double.isNaN(cos)) {
+			return Rotation2.ZERO;
+		}
+
+		return Rotation2.fromRadians(Math.acos(MathUtils.limit(cos, -1.0, 1.0)));
+	}
+
+	public Rotation2 getAngle() {
+		return new Rotation2(x, y, true);
 	}
 
 	public Vector2 add(Vector2 vector) {
@@ -48,10 +91,6 @@ public final class Vector2 {
 		return new Vector2(this.x + x, this.y + y);
 	}
 
-	public Vector2 subtract(double scalar) {
-		return subtract(scalar, scalar);
-	}
-
 	public Vector2 subtract(Vector2 vector) {
 		return subtract(vector.x, vector.y);
 	}
@@ -60,6 +99,18 @@ public final class Vector2 {
 		return new Vector2(this.x - x, this.y - y);
 	}
 
+	public Vector2 scale(double scalar) {
+		return multiply(scalar, scalar);
+	}
+
+	/**
+	 * Multiply each component of the vector by a scalar value.
+	 * @param scalar The scalar to multiply each component by.
+	 * @return The vector multiplied by the scalar.
+	 *
+	 * @deprecated Use {@link #scale(double)} instead.
+	 */
+	@Deprecated
 	public Vector2 multiply(double scalar) {
 		return multiply(scalar, scalar);
 	}
@@ -80,6 +131,22 @@ public final class Vector2 {
 		return new Vector2(x / length, y / length);
 	}
 
+	public double dot(Vector2 other) {
+		return x * other.x + y * other.y;
+	}
+
+	public double cross(Vector2 other) {
+		return x * other.y - y * other.x;
+	}
+
+	/**
+	 * Rotate this vector by the specified angle.
+	 * @param radians The angle the vector should be rotated by in radians.
+	 * @return The rotated vector.
+	 *
+	 * @deprecated Use {@link #rotateBy(Rotation2)} instead.
+	 */
+	@Deprecated
 	public Vector2 rotateBy(double radians) {
 		double cos = Math.cos(radians);
 		double sin = Math.sin(radians);
@@ -87,16 +154,33 @@ public final class Vector2 {
 		return new Vector2(x * cos - y * sin, x * sin + y * cos);
 	}
 
+	/**
+	 * Rotate this vector by the specified rotation.
+	 * @param rotation How much the vector should be rotated.
+	 * @return The rotated vector.
+	 */
+	public Vector2 rotateBy(Rotation2 rotation) {
+		return new Vector2(x * rotation.cos - y * rotation.sin, x * rotation.sin + y * rotation.cos);
+	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if (!(obj instanceof Vector2))
+		if (!(obj instanceof Vector2)) {
 			return false;
+		}
+
 		Vector2 other = (Vector2) obj;
 		return epsilonEquals(x, other.x) && epsilonEquals(y, other.y);
 	}
 
 	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+
+	@Override
 	public String toString() {
-		return String.format("(%.3f, %.3f)", x, y);
+		DecimalFormat fmt = new DecimalFormat("#0.000");
+		return '(' + fmt.format(x) + ", " + fmt.format(y) + ')';
 	}
 }
