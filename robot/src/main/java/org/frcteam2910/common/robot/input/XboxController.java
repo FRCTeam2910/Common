d1e80129e27162fe60b092f13c10bfc902416f78
Kaboomboom3 <Jacob@Bublitz.com>
2018-07-14 13:59:47 -0700

    Add maven publishing (#1)
    * Add vscode to gitignore
    
    * Add publishing to local maven repository
    
    * Update Gradle version
    
    * Use maven-publish plugin instead of maven plugin
    
    * Change version to 0.1.0 because it is currently in alpha
    
    * Add readme explaining how to use the libraries as dependencies

--- robot/src/main/java/org/frcteam2910/common/robot/input/XboxController.java
+++ robot/src/main/java/org/frcteam2910/common/robot/input/XboxController.java
@@ -6,13 +6,11 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * <p>IGamepad implementation for XBox-like gamepads.</p>
- * <p>
  * <p>Currently known to work with:
  * <ul>
  * <li>Xbox 360 wired controller</li>
  * <li>Logitech F310</li>
  * </ul>
- * </p>
  *
  * @author Jacob Bublitz
  * @since 1.0
