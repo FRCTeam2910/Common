89cfaf47256ccc6b4beb5a9f03ecd4207afb27b0
Jacob Bublitz <Jacob@Bublitz.com>
2018-08-30 10:03:49 -0700

    Remove duplicate python script

--- trajectory.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-data = np.genfromtxt('trajectory.csv', names=True, delimiter=',')
-
-plt.plot(data['time'], data['rotation'])
-plt.show()
