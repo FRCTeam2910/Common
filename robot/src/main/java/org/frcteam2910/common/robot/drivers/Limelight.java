09765a1d6d34f516641fb2394271e5b8cfe50682
Kaboomboom3 <Jacob@Bublitz.com>
2019-01-12 15:09:47 -0800

    Update Limelight wrapper to new limelight api version (#26)
    * Update Limelight wrapper to new limelight api version
    
    * Fixes cancer indentation

--- robot/src/main/java/org/frcteam2910/common/robot/drivers/Limelight.java
+++ robot/src/main/java/org/frcteam2910/common/robot/drivers/Limelight.java
@@ -7,97 +7,126 @@ import org.frcteam2910.common.math.MathUtils;
 import org.frcteam2910.common.math.Vector2;
 
 public final class Limelight {
-	private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    private final NetworkTable table;
 
-	private final NetworkTableEntry tv = table.getEntry("tv");
-	private final NetworkTableEntry tx = table.getEntry("tx");
-	private final NetworkTableEntry ty = table.getEntry("ty");
-	private final NetworkTableEntry ta = table.getEntry("ta");
-	private final NetworkTableEntry ts = table.getEntry("ts");
-	private final NetworkTableEntry tl = table.getEntry("tl");
+    private final NetworkTableEntry tv;
+    private final NetworkTableEntry tx;
+    private final NetworkTableEntry ty;
+    private final NetworkTableEntry ta;
+    private final NetworkTableEntry ts;
+    private final NetworkTableEntry tl;
 
-	private final NetworkTableEntry ledMode = table.getEntry("ledMode");
-	private final NetworkTableEntry camMode = table.getEntry("camMode");
-	private final NetworkTableEntry pipeline = table.getEntry("pipeline");
-	private final NetworkTableEntry stream = table.getEntry("stream");
-	private final NetworkTableEntry snapshot = table.getEntry("snapshot");
+    private final NetworkTableEntry ledMode;
+    private final NetworkTableEntry camMode;
+    private final NetworkTableEntry pipeline;
+    private final NetworkTableEntry stream;
+    private final NetworkTableEntry snapshot;
 
-	public boolean hasTarget() {
-		return MathUtils.epsilonEquals(tv.getDouble(0), 1);
-	}
+    public Limelight() {
+        this("limelight");
+    }
 
-	public double getTargetArea() {
-		return ta.getDouble(0);
-	}
+    public Limelight(String name) {
+        table = NetworkTableInstance.getDefault().getTable(name);
 
-	public Vector2 getTargetPosition() {
-		return new Vector2(tx.getDouble(0), ty.getDouble(0));
-	}
+        tv = table.getEntry("tv");
+        tx = table.getEntry("tx");
+        ty = table.getEntry("ty");
+        ta = table.getEntry("ta");
+        ts = table.getEntry("ts");
+        tl = table.getEntry("tl");
 
-	public double getTargetSkew() {
-		return ts.getDouble(0);
-	}
+        ledMode = table.getEntry("ledMode");
+        camMode = table.getEntry("camMode");
+        pipeline = table.getEntry("pipeline");
+        stream = table.getEntry("stream");
+        snapshot = table.getEntry("snapshot");
+    }
 
-	public void setCamMode(CamMode mode) {
-		switch (mode) {
-			case VISON:
-				camMode.setNumber(0);
-				break;
-			case DRIVER:
-				camMode.setNumber(1);
-		}
-	}
+    public boolean hasTarget() {
+        return MathUtils.epsilonEquals(tv.getDouble(0), 1);
+    }
 
-	public void setLedMode(LedMode mode) {
-		switch (mode) {
-			case ON:
-				ledMode.setNumber(0);
-				break;
-			case OFF:
-				ledMode.setNumber(1);
-				break;
-			case BLINK:
-				ledMode.setNumber(2);
-		}
-	}
+    public double getTargetArea() {
+        return ta.getDouble(0);
+    }
 
-	public void setSnapshotsEnabled(boolean enabled) {
-		if (enabled)
-			snapshot.setNumber(1);
-		else
-			snapshot.setNumber(0);
-	}
+    public Vector2 getTargetPosition() {
+        return new Vector2(Math.toRadians(tx.getDouble(0)), Math.toRadians(ty.getDouble(0)));
+    }
 
-	public void setPipeline(int pipeline) {
-		this.pipeline.setNumber(pipeline);
-	}
+    public double getTargetSkew() {
+        return ts.getDouble(0);
+    }
 
-	public void setStreamMode(StreamMode mode) {
-		switch (mode) {
-			case STANDARD:
-				stream.setNumber(0);
-				break;
-			case PIP_MAIN:
-				stream.setNumber(1);
-				break;
-			case PIP_SECONDARY:
-				stream.setNumber(2);
-				break;
-		}
-	}
+    public void setCamMode(CamMode mode) {
+        switch (mode) {
+            case VISION:
+                camMode.setNumber(0);
+                break;
+            case DRIVER:
+                camMode.setNumber(1);
+        }
+    }
 
-	public enum CamMode {
-		VISON,
-		DRIVER
-	}
+    public void setLedMode(LedMode mode) {
+        switch (mode) {
+            case DEFAULT:
+                ledMode.setNumber(0);
+                break;
+            case OFF:
+                ledMode.setNumber(1);
+                break;
+            case BLINK:
+                ledMode.setNumber(2);
+                break;
+            case ON:
+                ledMode.setNumber(3);
+                break;
+        }
+    }
 
-	public enum LedMode {
-		ON, OFF, BLINK
-	}
+    public void setSnapshotsEnabled(boolean enabled) {
+        if (enabled) {
+            snapshot.setNumber(1);
+        } else {
+            snapshot.setNumber(0);
+        }
+    }
 
-	public enum StreamMode {
-		STANDARD,
-		PIP_MAIN,
-		PIP_SECONDARY
-	}
+    public void setPipeline(int pipeline) {
+        this.pipeline.setNumber(pipeline);
+    }
+
+    public void setStreamMode(StreamMode mode) {
+        switch (mode) {
+            case STANDARD:
+                stream.setNumber(0);
+                break;
+            case PIP_MAIN:
+                stream.setNumber(1);
+                break;
+            case PIP_SECONDARY:
+                stream.setNumber(2);
+                break;
+        }
+    }
+
+    public enum CamMode {
+        VISION,
+        DRIVER
+    }
+
+    public enum LedMode {
+        DEFAULT,
+        ON,
+        OFF,
+        BLINK
+    }
+
+    public enum StreamMode {
+        STANDARD,
+        PIP_MAIN,
+        PIP_SECONDARY
+    }
 }
