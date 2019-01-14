61aa16a92fcab24aadb51a166dd80e4bb93181bd
Jacob Bublitz <jacob@bublitz.com>
2019-01-13 18:58:52 -0800

    Updates swerve module api
    Updates the swerve module api to make it easier to implement

--- src/test/java/org/frcteam2910/common/drivers/MockSwerveModule.java
+++ src/test/java/org/frcteam2910/common/drivers/MockSwerveModule.java
@@ -1,91 +1,56 @@
 package org.frcteam2910.common.drivers;
 
 import org.frcteam2910.common.math.MathUtils;
-import org.frcteam2910.common.math.Rotation2;
 import org.frcteam2910.common.math.Vector2;
 
 import static org.junit.Assert.assertTrue;
 
 public final class MockSwerveModule extends SwerveModule {
-	private double angleRotations;
-	private double targetAngleRotations;
-	private double driveDistance;
-	private double driveRate;
-	private double targetDrivePercentage;
-	private boolean driveInverted;
+	private double angle;
+	private double distance;
+	private double targetAngle;
+	private double driveOutput;
 
-	public MockSwerveModule(Vector2 modulePosition, Rotation2 adjustmentAngle) {
-		super(modulePosition, adjustmentAngle);
+	public MockSwerveModule(Vector2 modulePosition) {
+		super(modulePosition);
 	}
 
 	@Override
-	protected double getAngleEncoderRotations() {
-		return angleRotations;
+	protected double readAngle() {
+		return angle;
 	}
 
-	public void setAngleRotations(double angleRotations) {
-		this.angleRotations = angleRotations;
-	}
-
-	@Override
-	public double getCurrentDistance() {
-		return driveDistance;
-	}
-
-	public void setCurrentDistance(double driveEncoderRotations) {
-		this.driveDistance = driveEncoderRotations;
+	public void writeAngle(double angle) {
+		this.angle = angle;
 	}
 
 	@Override
-	public double getCurrentRate() {
-		return driveRate;
+	public double readDistance() {
+		return distance;
 	}
 
-	public void setCurrentRate(double driveEncoderRate) {
-		this.driveRate = driveEncoderRate;
-	}
-
-	public double getTargetAngleRotations() {
-		return targetAngleRotations;
+	public void writeDistance(double distance) {
+		this.distance = distance;
 	}
 
 	@Override
-	protected void setTargetAngleRotations(double rotations) {
-		this.targetAngleRotations = rotations;
+	protected void setTargetAngle(double angle) {
+		this.targetAngle = angle;
 	}
 
-	public boolean isDriveInverted() {
-		return driveInverted;
-	}
-
-	@Override
-	protected void setDriveMotorInverted(boolean inverted) {
-		this.driveInverted = inverted;
+	public double getTargetAngle() {
+		return targetAngle;
 	}
 
 	@Override
-	public void zeroDistance() {
-		this.driveDistance = 0;
+	public void setDriveOutput(double output) {
+		assertTrue("Drive output of swerve module is not in the range [-1, 1]",
+				MathUtils.isInRange(-1.0, 1.0, output));
+
+		this.driveOutput = output;
 	}
 
-	@Override
-	public double getCurrentDrivePercentage() {
-		return targetDrivePercentage;
-	}
-
-	public double getTargetDrivePercentage() {
-		return targetDrivePercentage;
-	}
-
-	@Override
-	public void setTargetDrivePercentage(double percentage) {
-		assertTrue("Target percentage of swerve module is not in the range [-1, 1]",
-				MathUtils.isInRange(-1, 1, percentage));
-
-		this.targetDrivePercentage = percentage;
-	}
-
-	public void accumulateRates(double deltaTime) {
-		driveDistance += driveRate * deltaTime;
+	public double getDriveOutput() {
+		return driveOutput;
 	}
 }
