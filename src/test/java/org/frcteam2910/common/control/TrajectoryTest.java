7501b701ddf227789e52ff96396cf046f91b8f99
Jacob Bublitz <jacob@bublitz.com>
2019-05-09 21:19:12 -0700

    Fixes indentation and removes extra comma

--- src/test/java/org/frcteam2910/common/control/TrajectoryTest.java
+++ src/test/java/org/frcteam2910/common/control/TrajectoryTest.java
@@ -65,7 +65,7 @@ public class TrajectoryTest {
         PATHS = new Path[]{
                 simplePath,
                 curvedPath,
-                splinePath,
+                splinePath
         };
     }
 
