8192b0cd16958644904d0441236b5407248bceb2
Jacob Bublitz <jacob@bublitz.com>
2019-10-29 16:56:56 -0700

    Add test to verify that constraints are being followed

--- src/test/java/org/frcteam2910/common/control2/TrajectoryTest.java
+++ src/test/java/org/frcteam2910/common/control2/TrajectoryTest.java
@@ -14,8 +14,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.Assert.assertThat;
+
 public class TrajectoryTest {
-    private static final double SAMPLE_DISTANCE = 1.0e-2;
+    private static final double SAMPLE_DISTANCE = 1.0e-3;
 
     private static final double DT = 5.0e-3;
 
@@ -23,15 +26,52 @@ public class TrajectoryTest {
     private static final double KA = 0.1;
     private static final double MAX_FEEDFORWARD = 10.0;
 
+    private static final double MAX_ACCELERATION = 30.0;
+    private static final double ALLOWABLE_ACCELERATION_ERROR = 1.0e-1;
+    private static final double ALLOWABLE_VELOCITY_ERROR = 1.0e-3;
+
     private static final Waypoint[] WAYPOINTS = {
             new Waypoint(new Vector2(0, 0), Rotation2.fromDegrees(90), Rotation2.fromDegrees(90)),
             new Waypoint(new Vector2(50.0, 50.0), Rotation2.fromDegrees(90), Rotation2.fromDegrees(0))
     };
     private static final ITrajectoryConstraint[] CONSTRAINTS = {
             new CentripetalAccelerationConstraint(7.5),
-            new MaxAccelerationConstraint(30.0),
+            new MaxAccelerationConstraint(MAX_ACCELERATION),
             new FeedforwardConstraint(MAX_FEEDFORWARD, KV, KA, true)
     };
+
+    @Test
+    public void obeysConstraints() {
+        Path path = new SplinePathBuilder()
+                .addWaypoints(WAYPOINTS)
+                .build();
+        Trajectory trajectory = new Trajectory(path, CONSTRAINTS, SAMPLE_DISTANCE);
+
+        int samples = (int) Math.ceil(trajectory.getDuration() / DT);
+        for (int i = 0; i <= samples; i++) {
+            Trajectory.State currentState = trajectory.calculate(i * DT);
+
+            double maximumVelocity = Double.POSITIVE_INFINITY;
+            for (ITrajectoryConstraint constraint : CONSTRAINTS) {
+                maximumVelocity = Math.min(maximumVelocity, constraint.getMaxVelocity(currentState.getPathState()));
+            }
+
+            assertThat("Velocity exceeded maximum velocity", maximumVelocity + ALLOWABLE_VELOCITY_ERROR,
+                    greaterThanOrEqualTo(Math.abs(currentState.getVelocity())));
+
+            double maximumAcceleration = Double.POSITIVE_INFINITY;
+            for (ITrajectoryConstraint constraint : CONSTRAINTS) {
+                if (currentState.getAcceleration() > 0.0) {
+                    maximumAcceleration = Math.min(maximumAcceleration, constraint.getMaxAcceleration(currentState.getPathState(), currentState.getVelocity()));
+                } else {
+                    maximumAcceleration = Math.min(maximumAcceleration, constraint.getMaxDeceleration(currentState.getPathState(), currentState.getVelocity()));
+                }
+            }
+
+            assertThat("Acceleration exceeded maximum acceleration", maximumAcceleration + ALLOWABLE_ACCELERATION_ERROR,
+                    greaterThanOrEqualTo(Math.abs(currentState.getAcceleration())));
+        }
+    }
 
     @Test
     public void speedTest() {
