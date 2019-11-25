f5126f4fe00a95a5144a01133be5ec4d709b199c
Jacob Bublitz <jacob@bublitz.com>
2019-11-24 17:25:14 -0800

    Adds example for new swerve kinematics

---
+++ examples/swerve-robot/src/main/java/frc/swerverobot/RobotMap.java
@@ -0,0 +1,24 @@
+package frc.swerverobot;
+
+public class RobotMap {
+    public static final int DRIVETRAIN_FRONT_LEFT_MODULE_ANGLE_ENCODER = 0;
+    public static final int DRIVETRAIN_FRONT_LEFT_MODULE_ANGLE_MOTOR = 0;
+    public static final double DRIVETRAIN_FRONT_LEFT_MODULE_ANGLE_OFFSET = -Math.toRadians(154.3 + 180.0);
+    public static final int DRIVETRAIN_FRONT_LEFT_MODULE_DRIVE_MOTOR = 0;
+
+    public static final int DRIVETRAIN_FRONT_RIGHT_MODULE_ANGLE_ENCODER = 0;
+    public static final int DRIVETRAIN_FRONT_RIGHT_MODULE_ANGLE_MOTOR = 0;
+    public static final double DRIVETRAIN_FRONT_RIGHT_MODULE_ANGLE_OFFSET = -Math.toRadians(329.0);
+    public static final int DRIVETRAIN_FRONT_RIGHT_MODULE_DRIVE_MOTOR = 0;
+
+    public static final int DRIVETRAIN_BACK_LEFT_MODULE_ANGLE_ENCODER = 0;
+    public static final int DRIVETRAIN_BACK_LEFT_MODULE_ANGLE_MOTOR = 0;
+    public static final double DRIVETRAIN_BACK_LEFT_MODULE_ANGLE_OFFSET = -Math.toRadians(218.1);
+    public static final int DRIVETRAIN_BACK_LEFT_MODULE_DRIVE_MOTOR = 0;
+
+    public static final int DRIVETRAIN_BACK_RIGHT_MODULE_ANGLE_ENCODER = 0;
+    public static final int DRIVETRAIN_BACK_RIGHT_MODULE_ANGLE_MOTOR = 0;
+    public static final double DRIVETRAIN_BACK_RIGHT_MODULE_ANGLE_OFFSET = -Math.toRadians(268.9 + 180.0);
+    public static final int DRIVETRAIN_BACK_RIGHT_MODULE_DRIVE_MOTOR = 0;
+
+}
